6c432308c55f0dcb3313a897c264eef5ecee8d53
Rafael Mendes <dossantosgit81@gmail.com>
2025-02-06 10:38:07 -0300

    Initial commit

---
+++ src/test/java/com/pizzariamarqlinda/api_pizzaria_marqlinda/ApiPizzariaMarqlindaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizzariamarqlinda.api_pizzaria_marqlinda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPizzariaMarqlindaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
