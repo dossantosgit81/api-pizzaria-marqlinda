ea087c20b328dea6f9bb4db0bfc51f9301acb936
Rafael Mendes <dossantosgit81@gmail.com>
2025-03-11 11:23:16 -0300

    Add unit tests login

--- src/main/java/com/pizzariamarqlinda/api_pizzaria_marqlinda/config/SecurityConfig.java
+++ src/main/java/com/pizzariamarqlinda/api_pizzaria_marqlinda/config/SecurityConfig.java
@@ -1,10 +1,5 @@
 package com.pizzariamarqlinda.api_pizzaria_marqlinda.config;
 
-import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -16,18 +11,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.security.KeyFactory;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
-import java.util.Base64;
 
 @Configuration
 @EnableWebSecurity
--- src/test/java/com/pizzariamarqlinda/api_pizzaria_marqlinda/service/LoginServiceTest.java
+++ src/test/java/com/pizzariamarqlinda/api_pizzaria_marqlinda/service/LoginServiceTest.java
@@ -1,95 +1,89 @@
 package com.pizzariamarqlinda.api_pizzaria_marqlinda.service;
 
-import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.OctetSequenceKey;
-import com.nimbusds.jose.jwk.source.ImmutableSecret;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.proc.SecurityContext;
-import com.pizzariamarqlinda.api_pizzaria_marqlinda.mapper.UserMapper;
-import com.pizzariamarqlinda.api_pizzaria_marqlinda.model.Cart;
+import com.pizzariamarqlinda.api_pizzaria_marqlinda.exception.ObjectNotFoundException;
 import com.pizzariamarqlinda.api_pizzaria_marqlinda.model.Role;
 import com.pizzariamarqlinda.api_pizzaria_marqlinda.model.User;
 import com.pizzariamarqlinda.api_pizzaria_marqlinda.model.dto.LoginReqDto;
 import com.pizzariamarqlinda.api_pizzaria_marqlinda.model.dto.LoginResDto;
 import com.pizzariamarqlinda.api_pizzaria_marqlinda.model.enums.ProfilesUserEnum;
-import com.pizzariamarqlinda.api_pizzaria_marqlinda.repository.UserRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
-import java.util.Optional;
 import java.util.Set;
 
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 @ExtendWith(MockitoExtension.class)
-public class LoginServiceTest {
-
-    @InjectMocks
-    private LoginService service;
+class LoginServiceTest {
 
     @Mock
     private UserService userService;
 
     @Mock
-    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
 
-    private PasswordEncoder encoder;
+    @Mock
+    private JwtEncoder jwtEncoder;
 
-    private User userSuccess;
-    LoginReqDto loginReqDtoSuccess;
+    @InjectMocks
+    private LoginService loginService;
+
+    private User user;
 
     @BeforeEach
-    void setup(){
-        encoder = new BCryptPasswordEncoder();
-        service.setJwtEncoder(jwtEncoder());
-        service.setEncoder(encoder);
-
-        userSuccess = User.builder()
-                .id(1L)
-                .cart(new Cart())
-                .name("Rafael")
-                .lastName("Santos")
-                .email("test@gmail.com")
-                .phone("123456789")
-                .password(encoder.encode("123456"))
-                .roles(Set.of(Role.builder().name(ProfilesUserEnum.COMMON_USER).id(1L).build()))
-                .build();
-        loginReqDtoSuccess = new LoginReqDto("test@gmail.com", "123456");
+    void setUp() {
+        loginService.setEncoder(passwordEncoder);
+        loginService.setJwtEncoder(jwtEncoder);
+        user = new User();
+        user.setId(1L);
+        user.setEmail("user@example.com");
+        user.setPassword("encodedPassword");
+        user.setRoles(Set.of(Role.builder().id(1L).name(ProfilesUserEnum.COMMON_USER).build()));
 
     }
 
     @Test
-    void shouldLogInSuccessfully(){
-        when(userService.findByEmail(anyString())).thenReturn(userSuccess);
-        LoginResDto res = service.login(loginReqDtoSuccess);
-        Assertions.assertNotNull(res.token());
-        Assertions.assertNotNull(res.expiresIn());
+    void shouldLoginSuccessfully() {
+        LoginReqDto loginReq = new LoginReqDto("user@example.com", "password");
+        when(userService.findByEmail(loginReq.email())).thenReturn(user);
+        when(passwordEncoder.matches(loginReq.password(), user.getPassword())).thenReturn(true);
+
+        Jwt mockJwt = mock(Jwt.class);
+        when(jwtEncoder.encode(any(JwtEncoderParameters.class))).thenReturn(mockJwt);
+        when(mockJwt.getTokenValue()).thenReturn("valid token");
+
+        LoginResDto response = loginService.login(loginReq);
+
+        assertNotNull(response);
+        assertNotNull(response.token());
+        assertEquals(300L, response.expiresIn());
     }
 
-    public static JwtEncoder jwtEncoder() {
-        // Define uma chave secreta para assinar o JWT
-        String secretKey = "my-super-secret-key-my-super-secret-key"; // Deve ter pelo menos 32 bytes
-       // SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
-
-        // Cria a chave JWK
-       // JWK jwk = new OctetSequenceKey.Builder(secretKeySpec).algorithm(com.nimbusds.jose.JWSAlgorithm.HS256).build();
-        JWKSource<SecurityContext> jwkSource = new ImmutableSecret<>(secretKey.getBytes());
-
-        // Retorna o JwtEncoder configurado
-        return new NimbusJwtEncoder(jwkSource);
+    @Test
+    void shouldThrowExceptionWhenPasswordIsInvalid() {
+        LoginReqDto loginReq = new LoginReqDto("user@example.com", "wrongpassword");
+        when(userService.findByEmail(loginReq.email())).thenReturn(user);
+        when(passwordEncoder.matches(loginReq.password(), user.getPassword())).thenReturn(false);
+        assertThrows(BadCredentialsException.class, () -> loginService.login(loginReq));
     }
 
+    @Test
+    void shouldThrowExceptionWhenUserNotFound() {
+        LoginReqDto loginReq = new LoginReqDto("unknown@example.com", "password");
+        when(userService.findByEmail(loginReq.email())).thenThrow(new ObjectNotFoundException("User not found"));
+
+        assertThrows(BadCredentialsException.class, () -> loginService.login(loginReq));
+    }
 }
