4fa00a206f3e614795f1c7bc71fae5901259fbe8
Rafael Mendes <dossantosgit81@gmail.com>
2025-03-30 13:28:37 -0300

    add observations in orderResDto

--- src/main/java/com/pizzariamarqlinda/api_pizzaria_marqlinda/model/dto/OrderReqDto.java
+++ src/main/java/com/pizzariamarqlinda/api_pizzaria_marqlinda/model/dto/OrderReqDto.java
@@ -1,13 +1,14 @@
 package com.pizzariamarqlinda.api_pizzaria_marqlinda.model.dto;
 
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record OrderReqDto(@NotBlank(message = "") String observations,
+public record OrderReqDto(String observations,
                           @NotNull(message = "Campo deve estar preenchido. ") Boolean delivery,
-                          @NotNull(message = "Campo deve estar preenchido. ") PaymentMethodReqIdDto paymentMethod,
-                          @NotNull(message = "Campo deve estar preenchido. ") AddressReqIdDto address) {
+                          @Valid @NotNull(message = "Campo deve estar preenchido. ") PaymentMethodReqIdDto paymentMethod,
+                          @Valid @NotNull(message = "Campo deve estar preenchido. ") AddressReqIdDto address) {
 
 }
 
--- src/main/java/com/pizzariamarqlinda/api_pizzaria_marqlinda/model/dto/OrderResDto.java
+++ src/main/java/com/pizzariamarqlinda/api_pizzaria_marqlinda/model/dto/OrderResDto.java
@@ -5,5 +5,5 @@ import com.pizzariamarqlinda.api_pizzaria_marqlinda.model.enums.StatusEnum;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record OrderResDto(Long id, StatusEnum status, BigDecimal total, LocalDateTime deliveryForecast, AddressResDto address) {
+public record OrderResDto(Long id, StatusEnum status, BigDecimal total, LocalDateTime deliveryForecast, AddressResDto address, String observations) {
 }
