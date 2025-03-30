76a080d5f8e7adc45925d23b883cafcaaf8a20e2
Rafael Mendes <dossantosgit81@gmail.com>
2025-03-30 14:03:27 -0300

    Adjusts clock

--- src/main/java/com/pizzariamarqlinda/api_pizzaria_marqlinda/config/DateConfig.java
+++ src/main/java/com/pizzariamarqlinda/api_pizzaria_marqlinda/config/DateConfig.java
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.Clock;
+import java.time.ZoneId;
 
 @Configuration
 public class DateConfig {
 
     @Bean
     public Clock clock(){
-        return Clock.systemDefaultZone();
+        ZoneId zoneId = ZoneId.of("America/Sao_Paulo");
+        return Clock.system(zoneId);
     }
 }
